b736f987f7cf72931a4e05f9a73bb4a268555427
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-10 09:10:52 -0700

    added initial icon

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css';
+import { FaRegHandRock } from 'react-icons/fa';
 
 const Main = () => {
   return (
@@ -9,12 +10,18 @@ const Main = () => {
           <label htmlFor='Score'>Score:</label>
           10
         </div>
+        <div className='hand'>
+          <FaRegHandRock className='initialIcon' />
+        </div>
       </div>
       <div className='timer'>Timer</div>
       <div className='player'>
         <div className='score'>
           <label htmlFor='Score'>Score:</label>
           10
+        </div>
+        <div className='hand'>
+          <FaRegHandRock className='initialIcon' />
         </div>
       </div>
     </div>
