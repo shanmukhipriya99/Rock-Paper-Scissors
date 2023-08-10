5452970248918ef20465d72985368885ce68277d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-10 11:59:43 -0700

    deciding winner

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,21 +8,56 @@ const Main = () => {
   const [runTimer, setRunTimer] = useState(false);
   const [playerScore, setPlayerScore] = useState(0);
   const [compScore, setCompScore] = useState(0);
+  const [compPlay, setCompPlay] = useState('');
+  const [playerPlay, setPlayerPlay] = useState('');
+  const [winner, setWinner] = useState('');
+
+  const options = [
+    { name: 'Rock', icon: '<FaHandRock />' },
+    { name: 'Paper', icon: '<FaHandPaper />' },
+    { name: 'Scissors', icon: '<FaHandScissors />' },
+  ];
 
   useEffect(() => {
     if (runTimer && timer > 0) {
       setTimeout(() => {
         setTimer(timer - 1);
-        console.log(timer);
       }, 1000);
     } else if (runTimer && timer <= 0) {
       setTimer(3);
       setRunTimer(false);
+      decideWinner();
     }
   }, [runTimer, timer]);
 
+  const computerPlay = () => {
+    const ind = Math.floor(Math.random() * 3);
+    setCompPlay(options[ind].name);
+  };
+
+  const decideWinner = () => {
+    if (compPlay === playerPlay) {
+      setWinner('Draw');
+    } else if (
+      (playerPlay === 'Rock' && compPlay === 'Scissors') ||
+      (playerPlay === 'Paper' && compPlay === 'Rock') ||
+      (playerPlay === 'Scissors' && compPlay === 'Paper')
+    ) {
+      setWinner('You win!');
+    } else {
+      setWinner('You lose!');
+    }
+    console.log('Winner:',compPlay, playerPlay);
+    setCompPlay('');
+    setPlayerPlay('');
+    console.log('Over:',compPlay, playerPlay);
+  };
+
   const selectedOptionHandler = (option) => {
-    if (option === 'Rock' || option === 'Paper' || option === 'Scissors') {
+    if (options[option]) {
+      setWinner('');
+      setPlayerPlay(options[option].name);
+      computerPlay();
       setRunTimer(true);
     }
   };
@@ -46,7 +81,7 @@ const Main = () => {
         {runTimer ? (
           <div className='timer'>{timer}</div>
         ) : (
-          <div className='timer'></div>
+          <div className='winner'>{winner}</div>
         )}
         <div className='player'>
           <div className='score'>
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,22 +3,23 @@ import './Options.css';
 import { FaHandRock, FaHandPaper, FaHandScissors } from 'react-icons/fa';
 
 const Options = ({ selectedOptionHandler }) => {
-  const [play, setPlay] = useState('');
+  const [play, setPlay] = useState();
 
   const optionHandler = (option) => {
     if (selectedOptionHandler) {
       selectedOptionHandler(option);
     }
+    setPlay();
   };
   return (
     <div className='optionsSelectionArea'>
       <div className='options'>
-        <FaHandRock onClick={() => setPlay('Rock')} />
-        <FaHandPaper onClick={() => setPlay('Paper')} />
-        <FaHandScissors onClick={() => setPlay('Scissors')} />
+        <FaHandRock onClick={() => setPlay(1)} />
+        <FaHandPaper onClick={() => setPlay(2)} />
+        <FaHandScissors onClick={() => setPlay(3)} />
       </div>
       {play ? (
-        <div className='play' onClick={() => optionHandler(play)}>
+        <div className='play' onClick={() => optionHandler(play-1)}>
           <span>Play</span>
         </div>
       ) : (
